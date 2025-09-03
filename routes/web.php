c8d947fd64cf578013649312583f57a84716602b
Miguel-dev23/test <araujofreitas2008@gmail.com>
2025-09-03 10:35:51 -0300

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/bakery', [PrincipalController::class, 'bakery']);
+
